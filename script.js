c4b8f1b8319c5ff88ab314ab690e641bf4a0b559
Michal Ciberej <michal.ciberej@gmail.com>
2023-05-20 22:29:12 +0200

    Finish computer input selector

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let playerSelection = "";
 let computerSelection = "";
-let playerScore = 0;
-let computerScore = 0;
 
 function getPlayerChoice() {
     let playerChoice = prompt("Choose Rock, Paper or Scissors",).toLowerCase();
@@ -22,50 +20,3 @@ function getComputerChoice() {
         return computerSelection = "scissors";
     }
 }
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        console.log("Tie!");
-}   else if (playerSelection == "rock" && computerSelection == "scissors") {
-        playerScore += 1;
-        console.log("Player wins this round - rock beats scissors");
-}   else if (playerSelection == "paper" && computerSelection == "rock") {
-        playerScore += 1;
-        console.log("Player wins this round - paper beats rock");
-}   else if (playerSelection == "scissors" && computerSelection == "paper") {
-        playerScore += 1;
-        console.log("Player wins this round - scissors beat paper");
-}   else if (computerSelection == "rock" && playerSelection == "scissors") {
-        computerScore += 1;
-        console.log("Computer wins this round - rock beats scissors");
-}   else if (computerSelection == "paper" && playerSelection == "rock") {
-        computerScore += 1;
-        console.log("Computer wins this round - paper beats rock");
-}   else {
-        computerScore += 1;
-        console.log("Computer wins this round - scissors beat paper")
-}}
-
-
-function reportWinner(playerScore, computerScore) {
-    if (playerScore > computerScore) {
-        console.warn("Player wins the game with " + playerScore + " points.");
-    }   else if (computerScore > playerScore) {
-        console.warn("Computer Wins the game with " + computerScore + " points." )
-    }   else {
-        console.warn("You both have same score. No one wins!")
-    }
-}
-
-
-function game() {
-    for (let i = 0; i <= 4; i++) {
-        getPlayerChoice()
-        getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log("Player - " + playerScore + " | " + "Computer - " + computerScore );
-    }
-    reportWinner(playerScore, computerScore)
-}
-
-game()
